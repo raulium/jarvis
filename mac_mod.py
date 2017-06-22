fb41ab967f6781583f6aa5a403a4c2ebd0a8f06b
Raul Harnasch <r.d.harnasch@gmail.com>
2017-06-21 20:57:50 -0400

    Added iCloudConnect to re-establish trust with pyicloud functions

--- mac_mod.py
+++ mac_mod.py
@@ -9,9 +9,37 @@ from subprocess import Popen, os, PIPE
 from pyicloud import PyiCloudService
 
 
+def iCloudConnect():
+    api = PyiCloudService(EMAIL)
+    if api.requires_2fa:
+        import click
+        print "Two-factor authentication required. Your trusted devices are:"
+
+        devices = api.trusted_devices
+        for i, device in enumerate(devices):
+            print "  %s: %s" % (i, device.get('deviceName',
+                "SMS to %s" % device.get('phoneNumber')))
+
+        device = click.prompt('Which device would you like to use?', default=0)
+        device = devices[device]
+        if not api.send_verification_code(device):
+            print "Failed to send verification code"
+            sys.exit(1)
+
+        code = click.prompt('Please enter validation code')
+        if not api.validate_verification_code(device, code):
+            print "Failed to verify verification code"
+            sys.exit(1)
+
+
 def macTerm(CMD):
     runcmd = ['sudo', '-u', USERNAME] + CMD
     Popen( runcmd )
+
+
+def appleScript(CMD):
+    c = ['osascript', '-e'] + CMD
+    macTerm(c)
 
 
 def setVolume(VALUE):
