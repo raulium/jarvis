7c4cdefe091bc6824df86aae556154087866e7bd
Raul Harnasch <r.d.harnasch@gmail.com>
2018-01-30 00:13:20 -0500

    I can't spell after midnight.

--- motivation_mod.py
+++ motivation_mod.py
@@ -33,7 +33,7 @@ def weeksToDate(dto):
 
 
 def motivate():
-    c = ['You can do it.', "I have full confidence you'll make it.", "You'll be fine.", "Cease the day!",
+    c = ['You can do it.', "I have full confidence you'll make it.", "You'll be fine.", "Seize the day!",
          "You've got this.", "Don't forget to breathe.", "Just do it.", "Failure is not an option.",
          "Resistance is futile.", "Hang in there."]
     return choice(c)
